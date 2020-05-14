bbaaa59ca37fa1830f214e081d58ccb803ad2cce
Hippolyte Cochin <hippolyte.cochin@gmail.com>
2020-05-14 12:57:10 +0200

    pause dej

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint_leaves = Ingredient.create(name: "mint leaves")
+
+puts "#{lemon.name} created"
+puts "#{ice.name} created"
+puts "#{mint_leaves.name} created"
